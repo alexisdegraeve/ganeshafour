1c5536a5f31ddedfe506d7db95d6fa1f39531818
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-13 20:05:16 +0200

    add board

--- ganeshafour/src/app/board/board.component.ts
+++ ganeshafour/src/app/board/board.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
+  myBoard: (number | null)[][]=new Array(6).fill(null).map(() => new Array(7).fill(null));
+
+  constructor() {
+  }
 
 }
