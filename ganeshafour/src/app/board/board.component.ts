b83ac0c2b43ff2dfb72548900b2273f3a462226b
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-13 20:33:54 +0200

    animate coin

--- ganeshafour/src/app/board/board.component.ts
+++ ganeshafour/src/app/board/board.component.ts
@@ -9,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class BoardComponent {
   myBoard: (number | null)[][]=new Array(6).fill(null).map(() => new Array(7).fill(null));
+  lastMove: { row: number, col: number } | null = null;
+
 
   constructor() {
   }
@@ -17,15 +19,21 @@ export class BoardComponent {
     const firstCellIndex = this.giveFirstEmpty(j);
     if(firstCellIndex !== null) {
       this.myBoard[firstCellIndex][j] = 1;
+      this.lastMove = { row: firstCellIndex, col: j };
     }
   }
 
   giveFirstEmpty(col: number): number | null {
+    if (col < 0 || col >= this.myBoard[0].length) return null;
     for (let index = this.myBoard.length - 1; index >= 0; index--) {
       if(this.myBoard[index][col] == null) {
         return index;
       }      
     }
     return null;
+  }
+
+  isJustPlayed(i: number, j: number): boolean {
+    return this.lastMove?.row === i && this.lastMove?.col === j;
   }
 }
