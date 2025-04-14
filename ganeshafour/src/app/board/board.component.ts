f0982ed98f012063640ee389f12118d1f6bc771e
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-14 11:44:37 +0200

    add checkwinner

--- ganeshafour/src/app/board/board.component.ts
+++ ganeshafour/src/app/board/board.component.ts
@@ -5,21 +5,22 @@ import { Component } from '@angular/core';
   selector: 'app-board',
   imports: [CommonModule],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
-  myBoard: (number | null)[][]=new Array(6).fill(null).map(() => new Array(7).fill(null));
-  lastMove: { row: number, col: number } | null = null;
+  myBoard: (number | null)[][] = new Array(6)
+    .fill(null)
+    .map(() => new Array(7).fill(null));
+  lastMove: { row: number; col: number } | null = null;
 
-
-  constructor() {
-  }
+  constructor() {}
   activeCell(i: number, j: number, cell: any) {
-    console.log(i, ' ', j , ' ',cell);
+    console.log(i, ' ', j, ' ', cell);
     const firstCellIndex = this.giveFirstEmpty(j);
-    if(firstCellIndex !== null) {
+    if (firstCellIndex !== null) {
       this.myBoard[firstCellIndex][j] = 1;
       this.lastMove = { row: firstCellIndex, col: j };
+      console.log('HUMAN : ' ,this.checkWinner(i, j, 1));
       this.computerPlay();
     }
   }
@@ -27,9 +28,9 @@ export class BoardComponent {
   giveFirstEmpty(col: number): number | null {
     if (col < 0 || col >= this.myBoard[0].length) return null;
     for (let index = this.myBoard.length - 1; index >= 0; index--) {
-      if(this.myBoard[index][col] == null) {
-        return index;        
-      }      
+      if (this.myBoard[index][col] == null) {
+        return index;
+      }
     }
     return null;
   }
@@ -41,28 +42,26 @@ export class BoardComponent {
   computerPlay() {
     console.log('Computert play');
     setTimeout(() => {
-        
-        // Trouver la ligne vide correspondate
-        // Placer le pion
-        // Mettre a jour last move
+      // Trouver la ligne vide correspondate
+      // Placer le pion
+      // Mettre a jour last move
 
-        // Rechercher une colonne dispo
-        let searchCol = this.giveFirstColFree();
-        if(searchCol !== null) {
-          const firstCellIndex = this.giveFirstEmpty(searchCol);
-          if(firstCellIndex !== null) {
-            this.myBoard[firstCellIndex][searchCol] = 2;
-            this.lastMove = { row: firstCellIndex, col: searchCol };
-          }
+      // Rechercher une colonne dispo
+      let searchCol = this.giveFirstColFree();
+      if (searchCol !== null) {
+        const firstCellIndex = this.giveFirstEmpty(searchCol);
+        if (firstCellIndex !== null) {
+          this.myBoard[firstCellIndex][searchCol] = 2;
+          this.lastMove = { row: firstCellIndex, col: searchCol };
+          console.log('COMPUTER : ' ,this.checkWinner(this.lastMove.row, this.lastMove.col, 2));
         }
-      
-        console.log('Computert finish to play');
+      }
 
-
-    }, 500)
+      console.log('Computert finish to play');
+    }, 500);
   }
 
-  giveFirstColFree(): number | null {    
+  giveFirstColFree(): number | null {
     for (let col = 0; col < this.myBoard[0].length; col++) {
       for (let row = this.myBoard.length - 1; row >= 0; row--) {
         if (this.myBoard[row][col] === null) {
@@ -71,5 +70,134 @@ export class BoardComponent {
       }
     }
     return null;
+  }
+
+  checkWinner(rowRef: number, colRef: number, player: number): boolean {
+    // Check Horizontale Right
+    if (this.checkHorizontalRight(rowRef, colRef, player)) {
+      return true;
+    }
+
+    // Check Horizontale Left
+    if (this.checkHorizontalLeft(rowRef, colRef, player)) {
+      return true;
+    }
+
+    // Check Verticale down
+    if (this.checkVerticalDown(rowRef, colRef, player)) {
+      return true;
+    }
+
+    // Check Verticale up
+    if (this.checkVerticalUp(rowRef, colRef, player)) {
+      return true;
+    }
+
+    // Check Diagonale down
+    if (this.checkDiagonaleDown(rowRef, colRef, player)) {
+      return true;
+    }
+
+    // Check Diagonale up
+    if (this.checkDiagonaleUp(rowRef, colRef, player)) {
+      return true;
+    }
+    return false;
+  }
+
+  checkHorizontalRight(
+    rowRef: number,
+    colDef: number,
+    player: number
+  ): boolean {
+    let cpt = 0;
+    for (let index = colDef; index < this.myBoard[0].length; index++) {
+      if (this.myBoard[rowRef][index] === player) {
+        cpt++;
+      }
+      if (cpt === 4) {
+        break;
+      }
+    }
+    if (cpt === 4) {
+      return true;
+    }
+    return false;
+  }
+
+  checkHorizontalLeft(rowRef: number, colDef: number, player: number): boolean {
+    let cpt = 0;
+    for (let index = colDef; index >= 0; index--) {
+      if (this.myBoard[rowRef][index] === player) {
+        cpt++;
+      }
+      if (cpt === 4) {
+        break;
+      }
+    }
+    if (cpt === 4) {
+      return true;
+    }
+    return false;
+  }
+
+  checkVerticalDown(rowRef: number, colDef: number, player: number): boolean {
+    let cpt = 0;
+    for (let index = rowRef; index < this.myBoard.length; index++) {
+      if (this.myBoard[index][colDef] === player) {
+        cpt++;
+      }
+      if (cpt === 4) {
+        break;
+      }
+    }
+    if (cpt === 4) {
+      return true;
+    }
+    return false;
+  }
+
+  checkVerticalUp(rowRef: number, colDef: number, player: number): boolean {
+    let cpt = 0;
+    for (let index = rowRef; index >= 0; index--) {
+      if (this.myBoard[index][colDef] === player) {
+        cpt++;
+      }
+      if (cpt === 4) {
+        break;
+      }
+    }
+    if (cpt === 4) {
+      return true;
+    }
+    return false;
+  }
+
+  checkDiagonaleDown(rowRef: number, colDef: number, player: number): boolean {
+    let cpt = 0;
+    let col = colDef;
+    let row = rowRef;
+    while (col < this.myBoard[0].length && row < this.myBoard.length) {
+      if (this.myBoard[row][col] === player) {
+        cpt++;
+      }
+      col++;
+      row++;
+    }
+    return cpt >= 4;
+  }
+
+  checkDiagonaleUp(rowRef: number, colDef: number, player: number): boolean {
+    let cpt = 0;
+    let col = colDef;
+    let row = rowRef;
+    while (col >= 0 && row >= 0) {
+      if (this.myBoard[row][col] === player) {
+        cpt++;
+      }
+      col--;
+      row--;
+    }
+    return cpt >= 4;
   }
 }
