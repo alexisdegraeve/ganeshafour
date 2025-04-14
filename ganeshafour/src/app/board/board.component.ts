0c1af08ad5a6640594c12089631686258fe6db8e
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-14 15:08:19 +0200

    fixing board

--- ganeshafour/src/app/board/board.component.ts
+++ ganeshafour/src/app/board/board.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -7,21 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss',
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
   myBoard: (number | null)[][] = new Array(6)
     .fill(null)
     .map(() => new Array(7).fill(null));
-  lastMove: { row: number; col: number } | null = null;
+  lastMove: { row: number | null; col: number | null } | null = null;
   startgame = false;
   winner: number | null = null;
 
   constructor() {}
+
+  ngOnInit(): void {}
 
   restart() {
     this.startgame = true;
     this.myBoard = new Array(6).fill(null).map(() => new Array(7).fill(null));
     this.lastMove = null;
     this.winner = null;
+
+    // this.myBoard[5][0] = 2;
+    // this.myBoard[5][1] = 2;
+    // this.myBoard[5][2] = 2;
+    // this.computerPlay();
   }
 
   activeCell(i: number, j: number, cell: any) {
@@ -30,9 +37,9 @@ export class BoardComponent {
     if (firstCellIndex !== null) {
       this.myBoard[firstCellIndex][j] = 1;
       this.lastMove = { row: firstCellIndex, col: j };
-      console.log('HUMAN : ' ,this.checkWinner(i, j, 1));
+      console.log('HUMAN : ', this.checkWinner(i, j, 1));
       this.winner = this.checkWinner(i, j, 1) ? 1 : null;
-      if(!this.winner) {
+      if (!this.winner) {
         this.computerPlay();
       } else {
         this.startgame = false;
@@ -62,18 +69,47 @@ export class BoardComponent {
       // Mettre a jour last move
 
       // Rechercher une colonne dispo
-      let searchCol = this.giveFirstColFree();
-      if (searchCol !== null) {
-        const firstCellIndex = this.giveFirstEmpty(searchCol);
-        if (firstCellIndex !== null) {
-          this.myBoard[firstCellIndex][searchCol] = 2;
-          this.lastMove = { row: firstCellIndex, col: searchCol };
-          this.winner = this.checkWinner(this.lastMove.row, this.lastMove.col, 2) ? 2 : null;
-          if(this.winner) {
-            this.startgame = false;
-          }
+      // let searchCol = this.giveFirstColFree();
+      // if (searchCol !== null) {
+      //   const firstCellIndex = this.giveFirstEmpty(searchCol);
+      //   if (firstCellIndex !== null) {
+      //     this.myBoard[firstCellIndex][searchCol] = 2;
+      //     this.lastMove = { row: firstCellIndex, col: searchCol };
+      //     this.winner = this.checkWinner(this.lastMove.row, this.lastMove.col, 2) ? 2 : null;
+      //     if(this.winner) {
+      //       this.startgame = false;
+      //     }
+      //   }
+      // }
+      if (!this.computerAttackPlay()) {
+        console.log('BLOCK Player');
+        if (!this.computerBlockPlayer()) {
+          this.randomComputerMove();
+        }
+      } else {
+        console.log('Attack Player');
+      }
+
+      if (this.lastMove && this.lastMove.row && this.lastMove.col) {
+        this.winner = this.checkWinner(this.lastMove.row, this.lastMove.col, 2)
+          ? 2
+          : null;
+        if (this.winner) {
+          this.startgame = false;
         }
       }
+
+      // if(this.computerBlockPlayer()) {
+      //   console.log('computer BLOCK PLAYER');
+      //   if(this.lastMove && this.lastMove.row && this.lastMove.col) {
+      //     this.winner = this.checkWinner(this.lastMove.row, this.lastMove.col, 2) ? 2 : null;
+      //     if(this.winner) {
+      //       this.startgame = false;
+      //     }
+      //   }
+      // } else {
+      //   console.log('computer ATTACK PLAYER');
+      // }
 
       console.log('Computert finish to play');
     }, 500);
@@ -92,17 +128,20 @@ export class BoardComponent {
 
   checkWinner(rowRef: number, colRef: number, player: number): boolean {
     // Check Horizontale
-    if (this.checkHorizontal(rowRef, colRef, player)>=4) {
+    if (this.checkHorizontal(rowRef, colRef, player) >= 4) {
       return true;
     }
 
     // Check Verticale
-    if (this.checkVertical(rowRef, colRef, player)>=4) {
+    if (this.checkVertical(rowRef, colRef, player) >= 4) {
       return true;
     }
 
     // Check Diagonale
-    if (this.checkDiagonaleUpLeft(rowRef, colRef, player)>=4 || (this.checkDiagonaleUpRight(rowRef, colRef, player)>=4)) {
+    if (
+      this.checkDiagonaleUpLeft(rowRef, colRef, player) >= 4 ||
+      this.checkDiagonaleUpRight(rowRef, colRef, player) >= 4
+    ) {
       return true;
     }
 
@@ -130,7 +169,6 @@ export class BoardComponent {
     return cpt;
   }
 
-
   checkVertical(rowRef: number, colDef: number, player: number): number {
     let cpt = 1;
     for (let index = rowRef + 1; index < this.myBoard.length; index++) {
@@ -150,7 +188,6 @@ export class BoardComponent {
     }
     return cpt;
   }
-
 
   checkDiagonaleUpLeft(rowRef: number, colDef: number, player: number): number {
     let cpt = 1;
@@ -180,7 +217,11 @@ export class BoardComponent {
     return cpt;
   }
 
-  checkDiagonaleUpRight(rowRef: number, colDef: number, player: number): number {
+  checkDiagonaleUpRight(
+    rowRef: number,
+    colDef: number,
+    player: number
+  ): number {
     let cpt = 1;
     let col = colDef + 1;
     let row = rowRef - 1;
@@ -208,16 +249,178 @@ export class BoardComponent {
     return cpt;
   }
 
-  computerBlockPlayer() {
+  computerBlockPlayer(): boolean {
+    for (let row = 0; row < this.myBoard.length; row++) {
+      for (let col = 0; col < this.myBoard[0].length; col++) {
+        // Vérifie chaque direction : HORIZONTALE, VERTICALE, DIAGONALES
 
+        // HORIZONTALE
+        if (this.checkHorizontal(row, col, 1) >= 3) {
+          // Tente de bloquer à droite
+          const right = col + 1;
+          if (right < this.myBoard[0].length) {
+            const emptyRow = this.giveFirstEmpty(right);
+            if (emptyRow !== null) {
+              this.myBoard[emptyRow][right] = 2;
+              this.lastMove = { row: emptyRow, col: right };
+              return true;
+            }
+          }
+          // Tente de bloquer à gauche
+          const left = col - 1;
+          if (left >= 0) {
+            const emptyRow = this.giveFirstEmpty(left);
+            if (emptyRow !== null) {
+              this.myBoard[emptyRow][left] = 2;
+              this.lastMove = { row: emptyRow, col: left };
+              return true;
+            }
+          }
+        }
+
+        // VERTICALE
+        if (this.checkVertical(row, col, 1) >= 3) {
+          const below = row + 1;
+          if (below < this.myBoard.length && this.myBoard[below][col] === 0) {
+            this.myBoard[below][col] = 2;
+            this.lastMove = { row: below, col: col };
+            return true;
+          }
+        }
+
+        // DIAGONALE UP-LEFT
+        if (this.checkDiagonaleUpLeft(row, col, 1) >= 3) {
+          const targetCol = col + 1;
+          const targetRow = row + 1;
+          if (
+            targetCol < this.myBoard[0].length &&
+            targetRow < this.myBoard.length &&
+            this.myBoard[targetRow][targetCol] === 0 &&
+            (targetRow === this.myBoard.length - 1 ||
+              this.myBoard[targetRow + 1][targetCol] !== 0)
+          ) {
+            this.myBoard[targetRow][targetCol] = 2;
+            this.lastMove = { row: targetRow, col: targetCol };
+            return true;
+          }
+        }
+
+        // DIAGONALE UP-RIGHT
+        if (this.checkDiagonaleUpRight(row, col, 1) >= 3) {
+          const targetCol = col - 1;
+          const targetRow = row + 1;
+          if (
+            targetCol >= 0 &&
+            targetRow < this.myBoard.length &&
+            this.myBoard[targetRow][targetCol] === 0 &&
+            (targetRow === this.myBoard.length - 1 ||
+              this.myBoard[targetRow + 1][targetCol] !== 0)
+          ) {
+            this.myBoard[targetRow][targetCol] = 2;
+            this.lastMove = { row: targetRow, col: targetCol };
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
   }
 
-  computerAttackPlay() {
+  computerAttackPlay(): boolean {
+    for (let row = 0; row < this.myBoard.length; row++) {
+      for (let col = 0; col < this.myBoard[0].length; col++) {
+        // Vérifie chaque direction : HORIZONTALE, VERTICALE, DIAGONALES
 
+        // HORIZONTALE
+        if (this.checkHorizontal(row, col, 2) >= 3) {
+          // Tente de bloquer à droite
+          console.log('ATTAQUE HORIZONTALEMENT');
+          console.log('bloaque a droite');
+          const right = col + 1;
+          if (right < this.myBoard[0].length) {
+            const emptyRow = this.giveFirstEmpty(right);
+            if (emptyRow !== null) {
+              console.log('emptyRow ', emptyRow);
+              console.log('right ', right);
+              this.myBoard[emptyRow][right] = 2;
+              this.lastMove = { row: emptyRow, col: right };
+              return true;
+            }
+          }
+
+          console.log('bloaque a gauche');
+          // Tente de bloquer à gauche
+          const left = col - 1;
+          if (left >= 0) {
+            const emptyRow = this.giveFirstEmpty(left);
+            if (emptyRow !== null) {
+              this.myBoard[emptyRow][left] = 2;
+              this.lastMove = { row: emptyRow, col: left };
+              return true;
+            }
+          }
+        }
+
+        // VERTICALE
+        if (this.checkVertical(row, col, 2) >= 3) {
+          const below = row + 1;
+          if (below < this.myBoard.length && this.myBoard[below][col] === 0) {
+            this.myBoard[below][col] = 2;
+            this.lastMove = { row: below, col: col };
+            return true;
+          }
+        }
+
+        // DIAGONALE UP-LEFT
+        if (this.checkDiagonaleUpLeft(row, col, 2) >= 3) {
+          const targetCol = col + 1;
+          const targetRow = row + 1;
+          if (
+            targetCol < this.myBoard[0].length &&
+            targetRow < this.myBoard.length &&
+            this.myBoard[targetRow][targetCol] === 0 &&
+            (targetRow === this.myBoard.length - 1 ||
+              this.myBoard[targetRow + 1][targetCol] !== 0)
+          ) {
+            this.myBoard[targetRow][targetCol] = 2;
+            this.lastMove = { row: targetRow, col: targetCol };
+            return true;
+          }
+        }
+
+        // DIAGONALE UP-RIGHT
+        if (this.checkDiagonaleUpRight(row, col, 2) >= 3) {
+          const targetCol = col - 1;
+          const targetRow = row + 1;
+          if (
+            targetCol >= 0 &&
+            targetRow < this.myBoard.length &&
+            this.myBoard[targetRow][targetCol] === 0 &&
+            (targetRow === this.myBoard.length - 1 ||
+              this.myBoard[targetRow + 1][targetCol] !== 0)
+          ) {
+            this.myBoard[targetRow][targetCol] = 2;
+            this.lastMove = { row: targetRow, col: targetCol };
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   }
 
   start() {
     this.restart();
   }
 
+  randomComputerMove() {
+    console.log("L'ordinateur joue de manière aléatoire");
+    const col = Math.floor(Math.random() * this.myBoard[0].length);
+    const row = this.giveFirstEmpty(col);
+    if (row !== null) {
+      this.myBoard[row][col] = 2;
+      this.lastMove = { row, col };
+    }
+  }
 }
